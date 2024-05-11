e39f46e9196b9ef92f90a6b27d1c6918f8841122
prism glue <169542506+prismglue@users.noreply.github.com>
2024-05-11 16:43:37 +0100

    add images from https://stock.adobe.com/images/313106435 resize images to fit mobile view make a deck of images find out z ordering makes cards disappear from renderer unless you add the orthographic camera plugin find out that input is kinda tricky without using colliders and an input plugin

---
+++ main/deck/card.go
@@ -0,0 +1,23 @@
+embedded_components {
+  id: "body"
+  type: "sprite"
+  data: "default_animation: \"club_ace_small\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
