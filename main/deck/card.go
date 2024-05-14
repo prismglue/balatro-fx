edc6e2ca2131b93e561ea192834440bb0b716f3c
prism glue <169542506+prismglue@users.noreply.github.com>
2024-05-14 13:24:58 +0100

    added 'selected' behaviour to cards, correctly cancel 'deal next card' behaviour after deal gosh, animating properties is nice and easy once you get used to callback contexts tried adding a shadow, fixed club_ace_small scaling moved the deck and hand zone a bit higher, can't see it on my smaller monitor

--- main/deck/card.go
+++ main/deck/card.go
@@ -85,3 +85,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "shadow"
+  type: "sprite"
+  data: "default_animation: \"card_shadow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
