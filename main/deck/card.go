d7db74308c925e44860e8a22a7d450ea590eb866
prism glue <169542506+prismglue@users.noreply.github.com>
2024-05-15 23:45:27 +0100

    disable cursor dragging because dragging trigger zones is kinda annoying add a script that re-enables dragging via messages started messing with hand sizes to see if the play_zone can reuse zone.script easier

--- main/deck/card.go
+++ main/deck/card.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "draggable"
+  component: "/main/draggable.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "body"
   type: "sprite"
